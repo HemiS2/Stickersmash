7c505ccf7e47aab6d507b6a165a38d2655e62059
HemiS2 <pedro.henrique000077@gmail.com>
2025-02-24 10:50:12 -0300

    vambora

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -3,10 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-    <Stack.Screen name="index" options={{ title: 'Home'}} />
-    <Stack
+      <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+    </Stack>
   )
- 
-
 }
 
